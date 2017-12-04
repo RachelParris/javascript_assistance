8b771cd6557ed82995ca052f7faa1ad4daa4d8ea
Ahmed Ismail <AHMED_ISMAIL@homedepot.com>
2017-12-04 17:00:01 -0500

    Add mysql connection example

--- session_3/app.js
+++ session_3/app.js
@@ -13,4 +13,22 @@ app.use('/api', api);
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
 
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'test',
+  password : 'test',
+  database : 'testDb'
+});
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
+
 module.exports = app;
